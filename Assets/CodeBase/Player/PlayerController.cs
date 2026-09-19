aebb2d8f88a154adea33dc83ec5c4e56e7ac8034
vid66 <v.khristoliubov@red-beat.com>
2026-09-19 22:49:25 +0300

    Move Player horizontally via CharacterController
    Read the direction from IInputService every frame, map it to the XZ plane, apply speed and Time.deltaTime and move the CharacterController. Assign the controller and move speed on the prefab.

--- Assets/CodeBase/Player/PlayerController.cs
+++ Assets/CodeBase/Player/PlayerController.cs
@@ -5,11 +5,25 @@ namespace CodeBase.Player
 {
     public class PlayerController : MonoBehaviour
     {
+        [SerializeField] private CharacterController _characterController;
+        [SerializeField] private float _moveSpeed;
         private IInputService _inputService;
 
         public void Construct(IInputService inputService)
         {
             _inputService = inputService;
+        }
+
+        private void Update()
+        {
+            Movement(_inputService.GetDirection());
+        }
+
+        private void Movement(Vector2 dir)
+        {
+            Vector3 moveDirection = new Vector3(dir.x, 0, dir.y);
+            Vector3 moveOffset = moveDirection * (_moveSpeed * Time.deltaTime);
+            _characterController.Move(moveOffset);
         }
     }
 }
